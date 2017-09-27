690400ad386a1d4cae80b1521c554f80851af9e4
Alex Elesin <aelesin@sfu.ca>
2017-09-27 14:15:38 -0700

    Add Shift methods
    Expanded what shift class could do, basic functionality implemented

--- src/main/java/Shift.java
+++ src/main/java/Shift.java
@@ -8,6 +8,54 @@ public class Shift {
     private Calendar startTime;
     private Calendar endTime;
     private Calendar date;
+    private User worker;
+
+    public Shift(Calendar startTime, Calendar endTime, Calendar date) {
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.date = date;
+    }
+
+    public Calendar getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Calendar startTime) {
+        this.startTime = startTime;
+    }
+
+    public Calendar getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Calendar endTime) {
+        this.endTime = endTime;
+    }
+
+    public Calendar getDate() {
+        return date;
+    }
+
+    public void setDate(Calendar date) {
+        this.date = date;
+    }
+
+    public void addWorker(User worker) {
+        this.worker = worker;
+    }
+
+    // Won't need to clear a worker because without someone to take their place it should be removed
+    public void replaceWorker(User worker) {
+        this.worker = worker;
+    }
+
+    // Needs to be thoroughly tested to make sure setting worker to null doesn't set that user's data to null
+    public void removeShift(){
+        startTime = null;
+        endTime = null;
+        date = null;
+        worker = null;
+    }
 
 
 }

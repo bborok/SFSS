d5b75895e29e039d22ec3ac0b5107932998f1a43
Patrick San Juan <psanjuan06.sj@gmail.com>
2017-10-24 12:30:18 -0700

    Starting

--- src/main/webapp/resources/js/schedule.js
+++ src/main/webapp/resources/js/schedule.js
@@ -2,167 +2,170 @@ var iBurnaby = ["Information and Lost & Found Kiosk", "Speed Watch/Moving Traffi
 var iSurrey = ["Community Presence", "Theft Prevention", "Special Events", "Pedestrian Safety"];
 var iVancouver = ["Community Presence", "Theft Prevention", "Special Events", "Pedestrian Safety"];
 
-$(document).ready(function() {
+$(document).ready(function () {
 
-            var events_array = [ //once we have database values, simple mysql is needed to connect values to these arrays
-            {
-                title: "Information and Lost & Found Kiosk",
-                id: "Burnaby Information and Lost & Found Kiosk",
-                allday: false,
-                member: "Bobae",
-                start: '2017-10-04T15:00:00',
-                end: '2017-10-04T15:30:00',
-                campus: 'Burnaby'
-            },
-            {
-                title: "Community Presence",
-                id: "Surrey Community Presence",
-                allday: false,
-                member: "Steven",
-                start: '2017-10-06T13:00:00',
-                end: '2017-10-06T15:00:00',
-                campus: 'Surrey'
-            },
-            {
-                title: "Pedestrian Safety",
-                id: "Vancouver Pedestrian Safety",
-                allday: false,
-                member: "Alex",
-                start: '2017-10-07T13:00:00',
-                end: '2017-10-07T15:00:00',
-                campus: 'Vancouver'
-            }
+    //Need to populate array via jaxCall
+    var events_array = [
+        {
+            title: "Information and Lost & Found Kiosk",
+            id: "Burnaby Information and Lost & Found Kiosk",
+            allday: false,
+            member: "Bobae",
+            start: '2017-10-04T15:00:00',
+            end: '2017-10-04T15:30:00',
+            campus: 'Burnaby'
+        },
+        {
+            title: "Community Presence",
+            id: "Surrey Community Presence",
+            allday: false,
+            member: "Steven",
+            start: '2017-10-06T13:00:00',
+            end: '2017-10-06T15:00:00',
+            campus: 'Surrey'
+        },
+        {
+            title: "Pedestrian Safety",
+            id: "Vancouver Pedestrian Safety",
+            allday: false,
+            member: "Alex",
+            start: '2017-10-07T13:00:00',
+            end: '2017-10-07T15:00:00',
+            campus: 'Vancouver'
+        }
 
-        ];
+    ];
 
 
-        // page is now ready, initialize the calendar...
+    // page is now ready, initialize the calendar...
 
-        $('#external-events .fc-event').each(function() {
+    $('#external-events .fc-event').each(function () {
 
-            // store data so the calendar knows to render an event upon drop
-            $(this).data('event', {
-                title: $.trim($(this).text()), // use the element's text as the event title
-                stick: true // maintain when user navigates (see docs on the renderEvent method)
-            });
-
-            // make the event draggable using jQuery UI
-            $(this).draggable({
-                zIndex: 999,
-                revert: true,      // will cause the event to go back to its
-                revertDuration: 0  //  original position after the drag
-            });
-
+        // store data so the calendar knows to render an event upon drop
+        $(this).data('event', {
+            title: $.trim($(this).text()), // use the element's text as the event title
+            stick: true // maintain when user navigates (see docs on the renderEvent method)
         });
 
+        // make the event draggable using jQuery UI
+        $(this).draggable({
+            zIndex: 999,
+            revert: true,      // will cause the event to go back to its
+            revertDuration: 0  //  original position after the drag
+        });
+    });
 
 
-        $('#calendar').fullCalendar({
-            // put your options and callbacks here
-            timezone: 'local',
-            viewRender: function (view) {
-                var h;
-                if (view.name == "month") {
-                    h = 700;
-                } else {
-                    h = 700;
-                }
-                $('#calendar').fullCalendar('option', 'contentHeight', h);
+    $('#calendar').fullCalendar({
+        // put your options and callbacks here
+        timezone: 'local',
+        viewRender: function (view) {
+            var h;
+            if (view.name == "month") {
+                h = 700;
+            } else {
+                h = 700;
+            }
+            $('#calendar').fullCalendar('option', 'contentHeight', h);
+        },
+
+        header: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'month,agendaWeek,agendaDay'
+        },
+
+        selectable: true,
+        events: events_array,
+
+        eventRender: function eventRender(event, element, view) {
+
+            if (event.campus == 'Burnaby') {
+                element.css('background-color', '#E8502F');
+            }
+            if (event.campus == "Surrey") {
+                element.css('background-color', '#C5E744');
+            }
+            if (event.campus == "Vancouver") {
+                element.css('background-color', '#75C6E7');
+            }
+            return filter(event);
+        },
+
+        eventAfterRender: function (event, element, view) {
+            $(element).css('width', '80%');
+        },
+
+        //Selecting an empty area
+        select: function (start, end, id) {
+
+            startTime = moment(start).format('MMM Do h:mm A');
+            endTime = moment(end).format('MMM Do h:mm A');
+
+            console.log(startTime +',' + endTime);
+            var mywhen = startTime + ' - ' + endTime;
+
+            $('#createEventModal #apptStartTime').val(start);
+            $('#createEventModal #apptEndTime').val(end);
+            $('#createEventModal #apptID').val(event.id);
+            $('#createEventModal #eventCampus').val(event.campus);
+            $('#createEventModal #eventMember').val(event.member);
+            $('#createEventModal #when').text(mywhen);
+            $('#createEventModal').modal('show'); //popup modal
+        },
+
+        //Selecting a scheduled event
+        eventClick: function (event) {
+
+            $('#modalTitle').html(event.title);
+            $('#modalStart').html(moment(event.start).format('MMM Do h:mm A'));
+            $('#modalEnd').html(moment(event.end).format('MMM Do h:mm A'));
+            $('#modalMember').html(event.member);
+            $('#modalCampus').html(event.campus);
+            $('#fullCalModal').modal();
+
+            $('#btnDelete').on('click', function (e) {
+                e.preventDefault();
+
+                $('#fullCalModal').modal('hide');
+                $('#calendar').fullCalendar('removeEvents', event._id);
+            })
+        },
+
+        navLinks: true, // can click day/week names to navigate views
+
+        weekNumbers: true,
+        weekNumbersWithinDays: true,
+        weekNumberCalculation: 'ISO',
+
+        editable: true
+    });
+
+    $('#submitButton').on('click', function (e) {
+        // We don't want this to act as a link so cancel the link action
+        e.preventDefault();
+
+        doSubmit();
+    });
+
+    function doSubmit() {
+
+        $("#createEventModal").modal('hide');
+        $("#calendar").fullCalendar('renderEvent',
+            {
+                title: $('#eventTitle').find(":selected").attr('class'),
+                // title: $('#eventTitle').val(),
+                start: new Date($('#apptStartTime').val()),
+                end: new Date($('#apptEndTime').val()),
+                allDay: ($('#apptAllDay').val() == "true"),
+                member: $('#eventMember').val(),
+                id: $('#eventTitle').val(),
+                campus: $('#eventCampus').val()
             },
 
-            header: {
-                left: 'prev,next today',
-                center: 'title',
-                right: 'month,agendaWeek,agendaDay'
-            },
-
-            selectable: true,
-            events: events_array,
-
-            eventRender: function eventRender( event, element, view ) {
-
-                if (event.campus == 'Burnaby') {
-                    element.css('background-color', '#E8502F');
-                }
-                if (event.campus == "Surrey") {
-                    element.css('background-color', '#C5E744');
-                }
-                if (event.campus == "Vancouver") {
-                    element.css('background-color', '#75C6E7');
-                }
-                return filter(event);
-            },
-
-            eventAfterRender: function(event, element, view) {
-                $(element).css('width','80%');
-            },
-
-            select: function (start, end, id) {
-
-                startTime = moment(start).format('MMM Do h:mm A');
-                endTime = moment(end).format('h:mm A');
-                var mywhen = startTime + ' - ' + endTime;
-
-                $('#createEventModal #apptStartTime').val(start);
-                $('#createEventModal #apptEndTime').val(end);
-                $('#createEventModal #apptID').val(event.id);
-                $('#createEventModal #eventCampus').val(event.campus);
-                $('#createEventModal #eventMember').val(event.member);
-                $('#createEventModal #when').text(mywhen);
-                $('#createEventModal').modal('show');
-            },
-
-            eventClick: function(event){
-
-                $('#modalTitle').html(event.title);
-                $('#modalStart').html(moment(event.start).format('MMM Do h:mm A'));
-                $('#modalEnd').html(moment(event.end).format('MMM Do h:mm A'));
-                $('#modalMember').html(event.member);
-                $('#modalCampus').html(event.campus);
-                $('#fullCalModal').modal();
-
-                $('#btnDelete').on('click', function(e) {
-                            e.preventDefault();
-
-                    $('#fullCalModal').modal('hide');
-                            $('#calendar').fullCalendar('removeEvents', event._id);
-                        })
-            },
-
-            navLinks: true, // can click day/week names to navigate views
-
-            weekNumbers: true,
-            weekNumbersWithinDays: true,
-            weekNumberCalculation: 'ISO',
-
-            editable: true
-        })
-
-        $('#submitButton').on('click', function(e){
-            // We don't want this to act as a link so cancel the link action
-            e.preventDefault();
-
-            doSubmit();
-        })
-
-        function doSubmit(){
-
-            $("#createEventModal").modal('hide');
-            $("#calendar").fullCalendar('renderEvent',
-                {
-                    title: $('#eventTitle').find(":selected").attr('class'),
-                    // title: $('#eventTitle').val(),
-                    start: new Date($('#apptStartTime').val()),
-                    end: new Date($('#apptEndTime').val()),
-                    allDay: ($('#apptAllDay').val() == "true"),
-                    member: $('#eventMember').val(),
-                    id: $('#eventTitle').val(),
-                    campus: $('#eventCampus').val()
-                },
-
-                true);
-            console.log($('#eventTitle').val());
-        }
+            true);
+        console.log($('#eventTitle').val());
+    }
 
     function filter(calEvent) {
 
@@ -189,11 +192,11 @@ $(document).ready(function() {
     }
 
 
-    $('input:checkbox.campusFilter').on('change', function() {
+    $('input:checkbox.campusFilter').on('change', function () {
         $('#calendar').fullCalendar('rerenderEvents');
     });
 
-    $('#shiftSelect').on('change', function() {
+    $('#shiftSelect').on('change', function () {
         $('#calendar').fullCalendar('rerenderEvents');
     });
 
@@ -208,7 +211,8 @@ $(document).ready(function() {
                 addItemsFromArray(eval("i" + this.id));
             });
         });
-        function addItemsFromArray (arr) {
+
+        function addItemsFromArray(arr) {
             $('#shiftSelect').append('<option value ="' + 'all' + '">' + 'All Shifts' + '</option>');
             $.each(arr, function (i, v) {
                 $("#shiftSelect").append('<option value="' + v + '">' + v + '</option>');
@@ -216,7 +220,7 @@ $(document).ready(function() {
         }
     });
 
-    $("#eventCampus").change(function() {
+    $("#eventCampus").change(function () {
         if ($(this).data('options') === undefined) {
             /*Taking an array of all options-2 and kind of embedding it on the select1*/
             $(this).data('options', $('#eventTitle option').clone());

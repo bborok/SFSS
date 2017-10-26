2b5a327d23ddb30658db95d0beb00c6f415b7286
Alex Elesin <aelesin@sfu.ca>
2017-10-26 00:59:51 -0700

    Removed UserService from being imported into IndexController
    UserService was removed from the project but wasn't removed from IndexController

--- src/main/java/com/zeta/Controllers/IndexController.java
+++ src/main/java/com/zeta/Controllers/IndexController.java
@@ -7,8 +7,6 @@ import com.zeta.Models.Login;
 import com.zeta.Models.Role;
 import com.zeta.Models.User;
 import com.zeta.Models.TimeCard;
-import com.zeta.Services.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;

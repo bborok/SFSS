85d50accbcb9a5c0a48bf5a0f29f63fe1f01f424
Amandeep Dhothar <adhothar@sfu.ca>
2017-11-14 14:28:41 -0800

    cleanup add user controller

--- src/main/java/com/zeta/Controllers/UserController.java
+++ src/main/java/com/zeta/Controllers/UserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/user")
 public class UserController {
     private UserData userData;
@@ -27,53 +27,30 @@ public class UserController {
     //Tested with URL:
     //localhost:8080/user/add?studentNumber=36&name=Eric&email=eric@sfu.ca&phoneNumber=656456789&role=team_lead&campus=surrey&accountCode=654
     @PostMapping("/add")
-    public @ResponseBody ResponseEntity addUserToDatabase(
-            @RequestParam("username") String username,
-            @RequestParam("studentNumber") long studentNumber,
-            @RequestParam("name") String name,
-            @RequestParam("email") String email ,
-            @RequestParam("phoneNumber") long phoneNumber,
-            @RequestParam("role") String role,
-            @RequestParam("preferredCampus") String campus,
-            @RequestParam("callSign") String callSign,
-            @RequestParam("training") List<Training> training,
-            @RequestParam("isDeactivated") Boolean isDeactivated){
-
-        User u = new User(
-                username,
-                studentNumber,
-                name,
-                email,
-                phoneNumber,
-                Role.valueOf(role.toUpperCase()),
-                Campus.valueOf(campus.toUpperCase()),
-                callSign,
-                training,
-                isDeactivated);
-
-        if (userData.addUser(u)) {
+    public ResponseEntity addUserToDatabase(@RequestBody User user){
+        if (userData.addUser(user)) {
             return new ResponseEntity(HttpStatus.OK);
         } else {
             return new ResponseEntity(HttpStatus.BAD_REQUEST);
         }
     }
 
-    @GetMapping("/{username}")
-    public String showUser(@PathVariable("username") String username, Model model){
-        User user = userData.getUser(username);
-        model.addAttribute("user", user);
-        return "/user/show";
-    }
-
-    @GetMapping("/showAll")
-    public String allUsers(Model model){
-        List<User> userList = userData.getAllUsers();
-        model.addAttribute(userList);
-        return "/user/showAll";
-    }
-
-    @GetMapping("/test")
-    public String testPage(){
-        return "/user/testPage";
-    }
+//    @GetMapping("/{username}")
+//    public String showUser(@PathVariable("username") String username, Model model){
+//        User user = userData.getUser(username);
+//        model.addAttribute("user", user);
+//        return "/user/show";
+//    }
+//
+//    @GetMapping("/showAll")
+//    public String allUsers(Model model){
+//        List<User> userList = userData.getAllUsers();
+//        model.addAttribute(userList);
+//        return "/user/showAll";
+//    }
+//
+//    @GetMapping("/test")
+//    public String testPage(){
+//        return "/user/testPage";
+//    }
 }

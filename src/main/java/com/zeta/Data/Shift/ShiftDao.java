20967586fba6e33c8c344c76ce9ba169b107f70e
Alex Elesin <aelesin@sfu.ca>
2017-11-20 14:52:10 -0800

    Added a try-catch to few Dao methods to catch exceptions

--- src/main/java/com/zeta/Data/Shift/ShiftDao.java
+++ src/main/java/com/zeta/Data/Shift/ShiftDao.java
@@ -36,27 +36,33 @@ public class ShiftDao implements ShiftData {
      */
     @Override
     public List<Shift> getShifts() {
-        String sql = "SELECT * FROM Shift ORDER BY Date DESC";
-        return jdbcTemplate.query(sql, new ShiftRowMapper());
+        try {
+            String sql = "SELECT * FROM Shift ORDER BY Date DESC";
+            return jdbcTemplate.query(sql, new ShiftRowMapper());
+        } catch (Exception e) {
+            return null;
+        }
     }
 
     @Override
     public List<Shift> getShiftsByUser(String username) {
-        List<Shift> list;
         try {
             String sql = "select * from Shift where User = ? order by Date desc";
-            list = jdbcTemplate.query(sql, new ShiftRowMapper());
+            return jdbcTemplate.query(sql, new ShiftRowMapper());
 
         } catch (Exception e) {
             return null;
         }
-        return list;
     }
 
     @Override
     public List<Shift> getShiftsWithSubmittedTimeCards() {
-        String sql = "SELECT * FROM Shift WHERE isTimeCardSubmitted = 1 ORDER BY Date DESC";
-        return jdbcTemplate.query(sql, new ShiftRowMapper());
+        try {
+            String sql = "SELECT * FROM Shift WHERE isTimeCardSubmitted = 1 ORDER BY Date DESC";
+            return jdbcTemplate.query(sql, new ShiftRowMapper());
+        } catch (Exception e) {
+            return null;
+        }
     }
 
     @Override

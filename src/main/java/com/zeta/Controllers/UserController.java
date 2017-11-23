a2b1a9633d197d4b061ca5e2040e4ee1b51efcc7
Amandeep Dhothar <adhothar@sfu.ca>
2017-11-23 01:29:45 -0800

    add ability to upload profile picture from profile page

--- src/main/java/com/zeta/Controllers/UserController.java
+++ src/main/java/com/zeta/Controllers/UserController.java
@@ -1,23 +1,28 @@
 package com.zeta.Controllers;
 
 import com.zeta.Data.User.UserData;
-import com.zeta.Models.Campus;
-import com.zeta.Models.Role;
-import com.zeta.Models.Training;
 import com.zeta.Models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
     private UserData userData;
+    private final String ROOT_PATH = System.getProperty("catalina.home");
+    private final String IMAGE_PATH = ROOT_PATH + File.separator + "images" + File.separator + "profiles";
+    private final File PROFILE_DIR = new File(IMAGE_PATH);
 
     @Autowired
     public UserController(UserData userData) {
@@ -27,7 +32,8 @@ public class UserController {
     //Tested with URL:
     //localhost:8080/user/add?studentNumber=36&name=Eric&email=eric@sfu.ca&phoneNumber=656456789&role=team_lead&campus=surrey&accountCode=654
     @PostMapping("/add")
-    public ResponseEntity addUserToDatabase(@RequestBody User user){
+    public ResponseEntity addUserToDatabase(@RequestBody User user, @RequestParam("file")MultipartFile file) {
+        uploadImage(file, user.getUsername());
         if (userData.getUser(user.getUsername()) == null) {
             if (userData.addUser(user)) {
                 return new ResponseEntity(HttpStatus.OK);
@@ -50,6 +56,41 @@ public class UserController {
         } else {
             return new ResponseEntity(HttpStatus.BAD_REQUEST);
         }
+    }
+
+    @PostMapping("/addImage")
+    public ResponseEntity uploadImageForUser(@RequestParam("userImage") MultipartFile file, HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("user");
+        uploadImage(file, user.getUsername());
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    private void uploadImage(MultipartFile file, String username) {
+        if (!file.isEmpty()) {
+            try {
+                byte[] bytes = file.getBytes();
+
+                // Creating the directory to store file
+                if (!PROFILE_DIR.exists()) {
+                    PROFILE_DIR.mkdirs();
+                }
+
+                // Create the file on server
+                File serverFile = new File(PROFILE_DIR.getAbsolutePath() + File.separator + username + ".png");
+                BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(serverFile));
+                stream.write(file.getBytes());
+                stream.close();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    @RequestMapping("/image/{username}")
+    public byte[] getProfileImage(@PathVariable("username") String username) throws IOException {
+        File image = new File(PROFILE_DIR.getAbsolutePath() + File.separator + username + ".png");
+        return Files.readAllBytes(image.toPath());
     }
 
 //    @GetMapping("/{username}")
